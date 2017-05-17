c8107c2501f49b69d5f9810be0b49ae031667466
Ankit Gaur <ruagtikna@gmail.com>
2017-05-17 23:22:01 +0530

    update

--- ipl.py
+++ ipl.py
@@ -1,5 +1,6 @@
 from cricbuzz import *
 from time import sleep
+import tags
 
 
 seriesName = "Indian Premier League, 2017"
@@ -7,24 +8,12 @@ seriesName = "Indian Premier League, 2017"
 currentBall = "0.0" #to track change in balls/over
 currentMatchStatus = "" #to track change in match status, if raining or whatever
 
-class Tags():
-	PlayingTeams = "Team"
-	BattingTeam = "Batting team"
-	BowlingTeam = "Bowling team"
-	BowlingTeamOvers = "Bowling Team Overs"
-	BattingTeamOvers = "Batting Team Overs"
-	BowlingTeamWickets = "Bowling Team Wickets" 
-	BattingTeamWickets = "Batting Team Wickets"
-	BowlingTeamRuns = "Bowling Team Runs"
-	BattingTeamRuns = "Batting Team Runs"
-	MatchState = "Match State"
-
 
 
 def ballChanged(match_detail):
 	global currentBall
-	if match_detail[Tags.BattingTeamOvers] != currentBall:
-		currentBall = match_detail[Tags.BattingTeamOvers]
+	if match_detail[tags.BattingTeamOvers] != currentBall:
+		currentBall = match_detail[tags.BattingTeamOvers]
 		return True
 	else :
 		return False	
@@ -32,11 +21,11 @@ def ballChanged(match_detail):
 def printProgressIfChanged(match_detail):
 	global currentMatchStatus
 	if currentMatchStatus == "":
-		currentMatchStatus = match_detail[Tags.MatchState]
+		currentMatchStatus = match_detail[tags.MatchState]
 		return 
-	if match_detail[Tags.MatchState] != currentMatchStatus:
-		currentMatchStatus = match_detail[Tags.MatchState]
-		print "Match Status Changed, "+match_detail[Tags.MatchState]
+	if match_detail[tags.MatchState] != currentMatchStatus:
+		currentMatchStatus = match_detail[tags.MatchState]
+		print "Match Status Changed, "+match_detail[tags.MatchState]
 
 
 def printIplDetails():
@@ -51,14 +40,14 @@ def printIplDetails():
 	        if match_detail is not None :
 	        	if ballChanged(match_detail):
 	        		print(chr(27) + "[2J") #clear screen
-	        		print match_detail[Tags.PlayingTeams]
-	        		print match_detail[Tags.BattingTeam] + " " + match_detail[Tags.BattingTeamRuns] + "/" + match_detail[Tags.BattingTeamWickets]
-	        		print "Overs: " + match_detail[Tags.BattingTeamOvers]
-	        		if match_detail[Tags.BowlingTeamRuns] is not None:
-	        			print "Target: "+ (int(match_detail[Tags.BowlingTeamRuns])+1) 
+	        		print match_detail[tags.PlayingTeams]
+	        		print match_detail[tags.BattingTeam] + " " + match_detail[tags.BattingTeamRuns] + "/" + match_detail[tags.BattingTeamWickets]
+	        		print "Overs: " + match_detail[tags.BattingTeamOvers]
+	        		if match_detail[tags.BowlingTeamRuns] is not None:
+	        			print "Target: "+ (int(match_detail[tags.BowlingTeamRuns])+1) 
 	        		else :
 	        			print "Batting First"
-	        		print 	match_detail[Tags.MatchState]
+	        		print 	match_detail[tags.MatchState]
 	        	printProgressIfChanged(match_detail) #print match status if it changed, delayed due to rain etc.			
 	        else :
 	        	print "match details not found"
